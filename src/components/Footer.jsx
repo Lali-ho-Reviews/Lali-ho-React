e2060a2164b536e16ecb0d42b13bac5e219b6521
Amelia <gcab022111@coderacademy.edu.au>
2022-07-14 08:03:27 +1000

    moved footer to its own component and added it back to the routes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        FINAL FANTASY is a registered trademark of Square Enix Holdings Co.,
+        Ltd.
+      </p>
+      <p>FINAL FANTASY XIV © SQUARE ENIX CO., LTD.</p>
+    </div>
+  )
+}
+
+export default Footer;

4a491067b58c9a7f4fa62d531bf686175b45da45
Amelia <gcab022111@coderacademy.edu.au>
2022-07-14 14:30:52 +1000

    added in new logo, styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,26 @@
 import React from "react";
 
+
 const Footer = () => {
   return (
-    <footer class="p-4 bg-fgrey rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <footer class="p-0 bg-fgrey rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
       <div class="sm:flex sm:items-center sm:justify-between">
-        <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+        <a class="flex items-center sm:mb-0">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="mr-3 h-8"
-            alt="Flowbite Logo"
+            src= "/logo3.png"
+            class=" h-10 pt-0 m-0"
+            alt="Lali-ho Logo"
           />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
         </a>
         <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">
+            <a href="#" class=" hover:underline md:mr-6 ">
               About
             </a>
           </li>
           <li>
             <a href="#" class="mr-4 hover:underline md:mr-6">
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">
-              Licensing
+              Github
             </a>
           </li>
           <li>
@@ -37,18 +30,13 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2022{" "}
-        <a href="https://flowbite.com/" class="hover:underline">
-          Lali-ho™
-        </a>
-        . All Rights Reserved.{" "} 
-       
-        <span class="block text-xs "></span>
-          FINAL FANTASY is a registered trademark of Square Enix Holdings Co.,
-          Ltd.
-       
+      
+      <hr class=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-1" />
+      <span class="block text-xs text-gray-500 sm:text-center dark:text-gray-400 ">
+        © 2022 Lali-ho™ . All Rights Reserved.{" "}
+        <span class="block text-xs pt-3 pb-0 xs:text-center "></span>
+        FINAL FANTASY is a registered trademark of Square Enix Holdings Co.,
+        Ltd.
         <p>FINAL FANTASY XIV © SQUARE ENIX CO., LTD.</p>
       </span>
     </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    colors:{
-      'fgrey': '#21252a'
-    }
+    colors: {
+      fgrey: "#21252a",
+    },
+    extend: {
+      fontSize: {
+        xs: ".5rem",
+      },
+    },
   },
   plugins: [],
-}
+};

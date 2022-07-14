c2aeb933a4638f6981bede79e99eb7189100ae1d
Amelia <gcab022111@coderacademy.edu.au>
2022-07-14 17:55:17 +1000

    editing bg styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,20 @@ import React from "react";
 
 
 function Home() {
+<<<<<<< Updated upstream
   return <h1 className="text-4xl text-center">Home</h1>;
+=======
+  return
+    
+
+
+
+
+
+
+  
+
+>>>>>>> Stashed changes
 }
 
 export default Home;

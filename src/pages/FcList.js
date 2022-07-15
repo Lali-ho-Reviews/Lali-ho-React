7728394a7233047f465ce54cb0378e2fb856c7a0
Amelia <gcab022111@coderacademy.edu.au>
2022-07-15 14:09:37 +1000

    merged the styling with react

--- src/pages/FcList.js
+++ src/pages/FcList.js
@@ -22,28 +22,10 @@ class FcList extends Component {
 
     return (
       <div>
-        <table>
-          <tr>
-            <td>FC NAME</td>
-            <td>Tag</td>
-            <td>Members</td> 
-            <td>Reviews</td>
-            <td>Link</td> 
-          </tr>
+        
         {this.state.data.map(fc => (
-          <tr>
-            <td>{fc.name}</td>
-            <td>{fc.slogan}</td>
-            <td>{fc.members}</td>
-            <td>{fc.rank}</td>
-
-            <td><a href={"/FcPage/" + fc.id}>Link</a></td>
-          </tr>
-        ))}
-        </table>
-
-
-        <div class="flex flex-col justify-center h-screen">
+         
+          <div class="flex flex-col justify-center h-screen">
           <div
             class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
             <div class="w-full md:w-1/3 bg-white grid place-items-center">
@@ -51,7 +33,7 @@ class FcList extends Component {
             </div>
               <div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
                 <div class="flex justify-between item-center">
-                  <p class="text-gray-500 font-medium hidden md:block">Rank:</p>
+                  <p class="text-gray-500 font-medium hidden md:block">Rank: {fc.rank}</p>
                   <div class="flex items-center">
                     <p class="text-blue-400 font-bold text-sm ml-1">
                       5 reviews 
@@ -59,20 +41,24 @@ class FcList extends Component {
                   </div>
                   
                   <div class=" px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
-                    Active members </div>
+                    Active members: {fc.members} </div>
                 </div>
-                <h3 class="font-black text-gray-800 md:text-3xl text-xl">Fc name</h3>
-                <p class="md:text-lg text-gray-500 text-base">Slogan</p>
+                <h3 class="font-black text-gray-800 md:text-3xl text-xl">{fc.name}</h3>
+                <p class="md:text-lg text-gray-500 text-base">{fc.slogan}</p>
                 <p class="text-xl font-black text-gray-800">
-                  Go to reviews 
+                <a href={"/FcPage/" + fc.id}>Link</a> 
                 </p>
               </div>
             </div>
           </div>
-          
-=======
->>>>>>> main
-      </div>
+          ))}
+     </div>
+
+        
+        
+
+
+       
     );
   }
 }

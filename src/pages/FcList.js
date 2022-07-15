85bfc5fe848eacb9955ef9cc0446c9fd86908d4c
Amelia <gcab022111@coderacademy.edu.au>
2022-07-15 14:56:50 +1000

    styled padding for even spacing

--- src/pages/FcList.js
+++ src/pages/FcList.js
@@ -25,25 +25,25 @@ class FcList extends Component {
         
         {this.state.data.map(fc => (
          
-          <div class="flex flex-col h-52">
+          <div class="flex flex-col pb-3 pt-1">
             <div
-              class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
+              class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-s md:max-w-3xl mx-auto border border-white bg-white">
             <div class="w-full md:w-1/3 bg-white grid place-items-center">
               <img src="/startscreen.jpeg" alt="ff14 loading screen backgorund" class="rounded-xl" />
             </div>
               <div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
                 <div class="flex justify-between item-center">
-                  <p class="text-gray-500 font-medium hidden md:block">Rank: {fc.rank}</p>
+                  <p class="text-gray-500 font-sm hidden md:block">Rank: {fc.rank}</p>
                   <div class="flex items-center">
                     <p class="text-blue-400 font-bold text-sm ml-1">
                       5 reviews 
                     </p>
                   </div>
                   
-                  <div class=" px-3 py-1 rounded-full text-xs font-medium text-gray-800 hidden md:block">
+                  <div class=" px-3 py-1 rounded-full text-sm font-sm text-gray-800 hidden md:block">
                     Active members: {fc.members} </div>
                 </div>
-                <h3 class="font-black text-gray-800 md:text-3xl text-xl">{fc.name}</h3>
+                <h3 class="font-black text-gray-800 md:text-3xl text-xl ">{fc.name}</h3>
                 <p class="md:text-lg text-gray-500 text-base">{fc.slogan}</p>
                 <p class="text-xl font-black text-blue-400">
                 <a href={"/Fc/" + fc.id}>See Reviews</a> 

38779795e854f07aafbb50f3bf64fc3dd315616a
Amelia <92557151+ameliaxjones@users.noreply.github.com>
2022-07-27 16:52:21 +1000

    Feature/styleforms (#41)
    * npm installed tailwind forms, styled sign up form
    
    * styled login form and edited text on sign up form

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -1,64 +1,100 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { signIn } from "../services/authServices"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signIn } from "../services/authServices";
 
-const LoginForm = ({activateUser}) => {
+const LoginForm = ({ activateUser }) => {
+  const navigate = useNavigate();
+  const initialFormData = {
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialFormData);
+  const [error, setError] = useState(null);
 
-    const navigate = useNavigate()
-    const initialFormData = {
-        email: "",
-        password: ""
-    }
-    const [formData, setFormData] = useState(initialFormData)
-    const [error, setError] = useState(null)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signIn(formData).then((user) => {
+      // console.log(user)
+      if (user.error) {
+        console.log("user.error", user.error);
+        setError(user.error);
+      } else {
+        setError(null);
+        sessionStorage.setItem("username", user.username);
+        sessionStorage.setItem("token", user.jwt);
+        console.log(sessionStorage.getItem("username"));
+        activateUser(user);
+        setFormData(initialFormData);
+        navigate("/fc");
+      }
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        signIn(formData)
-        .then((user) => {
-            // console.log(user)
-            if(user.error){
-                console.log("user.error", user.error)
-                setError(user.error)
-            }else{
-                setError(null)
-                sessionStorage.setItem("username",  user.username)
-                sessionStorage.setItem("token", user.jwt)
-                console.log(sessionStorage.getItem("username"))
-                activateUser(user)
-                setFormData(initialFormData)
-                navigate("/fc")
-            }
-            
-        })
-    }
+  const handleFormData = (e) => {
+    console.log(e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    const handleFormData = (e) => {
-        console.log(e.target.value)
-        setFormData({
-            ...formData,
-            [e.target.id]: e.target.value
-        })
-    }
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
 
 
-    return (
-        <>
-        {error && <p>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" name="email" id="email" value={formData.email} onChange={handleFormData} />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" value={formData.password} onChange={handleFormData} />
-                </div>
+        <input type="submit" value="Login" />
+        <div class="bg-grey-lighter min-h-screen flex flex-col">
+          <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+            <div class="bg-white px-6 py-8 rounded shadow-md text-blue-500 w-full">
+              <h1 class="mb-8 text-3xl text-center">Sign in</h1>
 
-                <input type="submit" value="Login" />
-            </form>
-        </>
-    )
-}
+              <label>Email:</label>
+              <input
+                type="email"
+                class="block border border-grey-light w-full p-3 rounded mb-4"
+                name="email"
+                id="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleFormData}
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                class="block border border-grey-light w-full p-3 rounded mb-4"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleFormData}
+              />
 
-export default LoginForm
+              <button
+                type="submit"
+                value="Login"
+                class="w-full text-center py-3 rounded bg-logoblue text-white hover:bg-green-dark focus:outline-none my-1"
+              >
+                Login
+              </button>
+            </div>
+
+            <div class="text-grey-dark mt-6">
+              Don't have an account?
+              <a
+                class="no-underline border-b border-blue text-blue-500"
+                href="/auth/signup"
+              >
+                Create one
+              </a>
+              .
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;

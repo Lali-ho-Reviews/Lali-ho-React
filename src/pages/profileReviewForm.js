97b1dbc9a42d69a8df888df4eb9b153c2a1cfd36
Amelia <gcab022111@coderacademy.edu.au>
2022-07-30 14:02:32 +1000

    reviews to work on account

--- src/pages/profileReviewForm.js
+++ src/pages/profileReviewForm.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function ProfileReviewForm() {
+
+
+function ProfileReviewForm(props) {
   return ( 
-    <>
+    <div>
       <h1> My Reviews! </h1>
-      <p></p>
-    </>
+        {props.value.reviews.map(review => (
+        <p>{review.text} {review.company_name} {review.rating}</p>
+        ))}
+    </div>
   )
 }
 

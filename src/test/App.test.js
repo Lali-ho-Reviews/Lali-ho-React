a0e125295c2e458ee2388197a9aee3ff07304c6e
Amelia <gcab022111@coderacademy.edu.au>
2022-07-30 12:25:36 +1000

    footer test and navigation test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../components/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders footer', () => {
+  const { getByText } = render (<App/>);
+  const linkElement = getByText(/about/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders navigation ', () => {
+  const { getByText } = render (<App/>);
+  const linkElement = getByText(/free companies/i);
   expect(linkElement).toBeInTheDocument();
 });

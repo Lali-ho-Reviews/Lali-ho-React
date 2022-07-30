717792ba692ef001c6b4f939fdd7c21dd7b784f2
Amelia <gcab022111@coderacademy.edu.au>
2022-07-30 13:39:41 +1000

    changed url names for. signin/login and profile to be user friendly, allowed acess to usernames from api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = ({loggedInUser, activateUser}) => {
             { !logged &&
               <li>
                 <a
-                  href="/auth/signin"
+                  href="/signin"
                   class="block py-2 pr-4 pl-3 text-white hover:bg-blue-700 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Sign In
@@ -85,7 +85,7 @@ const Navbar = ({loggedInUser, activateUser}) => {
             { !logged &&
               <li>
                 <a
-                  href="/auth/signup"
+                  href="/signup"
                   class="block py-2 pr-4 pl-3 text-white hover:bg-blue-700 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Sign Up
@@ -96,7 +96,7 @@ const Navbar = ({loggedInUser, activateUser}) => {
               <li>
                 <a
 
-                  href="/profile/:username"
+                  href="/account"
 
                   class="block py-2 pr-4 pl-3 text-white hover:bg-blue-700 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white md:dark:hover:bg-transparent"
                 >

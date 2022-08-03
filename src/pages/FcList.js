8bd6d0ad5686ab23bd0ae35270673bbe91366d53
Harley-ca <92524806+harley-ca@users.noreply.github.com>
2022-08-03 15:24:17 +1000

    Working search and XIV integration ready

--- src/pages/FcList.js
+++ src/pages/FcList.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import ffxxivApi from "../api/ffxxivApi";
 import lalihoApi from "../api/lalihoApi";
-
+import FcItem from "../components/FcItem"
 
 // 
 function FcList() {
@@ -13,8 +14,10 @@ function FcList() {
   const { search } = window.location;
   const query = new URLSearchParams(search).get('s');
   let urlSuffixLaliho = ""
+  let urlSuffixXiv = ""
   if (query) {
     urlSuffixLaliho = `search/${query}`
+    urlSuffixXiv = `search?name=${query}&columns=Name,ID,Server`
   }
 
 
@@ -25,13 +28,20 @@ function FcList() {
       .get("/companies/" + urlSuffixLaliho)
       .then(response => response.data)
       .catch((error) => console.error(error));
-    // const xivResponse = await xivApi
-    //   .get 
+    // const xivResponse = await ffxxivApi
+    //   .get("/freecompany/" + urlSuffixXiv)
+    //   .then(response => response.data.Results.map((company) => {
+    //     return {
+    //       name: company.Name,
+    //       ff_id: company.ID,
+    //       server: company.Server
+    //     }
+    //   }))
+    //   .then(response => console.log(response))
     //   .catch((error) => console.error(error));
     setData({
       companies: lalihoResponse,
-      xiv_companies: {},
-      company_list: {}
+      xiv_companies: {}
     });
   }
   // useEffect implemented to call fetchData on page load, empty array applied to the end to avoid DDOS Attack on the backend and avoid loop
@@ -41,39 +51,14 @@ function FcList() {
 
   return (
     <div>
-      {query && data.companies.length > 0 && <p>Showing results for "{query}"</p>}
-      {data.companies.map(fc => (
-         
-         <div class="flex flex-col pb-3 pt-1" key={fc.id}>
-           <div
-             class="relative flex flex-col md:flex-row md:space-x-5 space-y-3  rounded-xl shadow-lg p-3 max-w-s md:max-w-lg mx-auto border border-fgrey bg-fgrey">
-
-           <div class="w-full md:w-1/6 bg-fgrey grid place-items-center">
-             <img src="/startscreen.jpeg" alt="ff14 loading screen backgorund" class="rounded-xl" />
-           </div>
-             <div class="w-full md:w-2/3 bg-fgrey flex flex-col space-y-2 p-3">
-               <div class="flex justify-between item-center">
-                 <p class="text-gray-400 text-xs hidden md:block">Rank: {fc.rank}</p>
-                 <div class="flex items-center">
-                   <p class="text-blue-400 font-bold text-xs ml-1">
-                     5 reviews 
-                   </p>
-                 </div>
-                 
-                 <div class=" px-3 py-1 rounded-full text-xs font-s text-gray-400 hidden md:block">
-                   Active members: {fc.members} </div>
-               </div>
-               <h3 class="font-black text-gray-300 md:text-l text-l ">{fc.name}</h3>
-               <p class="md:text-m text-gray-500 text-base">{fc.slogan}</p>
-               <p class="text-s font-black text-blue-400">
-               <a href={"/Fc/" + fc.id}>See Reviews</a> 
-               </p>
-             </div>
-           </div>
-         </div>
-         ))}
-         { data.companies.length < 1 && <p>Sorry! We couldn't find any Free Companies with the search term "{query}"</p>}
-   </div>
+      {query && data.companies && <p>Showing results for "{query}"</p>}
+      {data.companies.map((fc) => (
+              
+              <FcItem data={fc} />
+          
+      ))}
+      { data.companies.length < 1 && <p>Sorry! We couldn't find any Free Companies with the search term "{query}"</p>}
+    </div>
   );
 }
 
